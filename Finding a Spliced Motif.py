56d9c37af0cee9adb58da5579a388facf04ff8f9
deMartino1908303 <115620752+deMartino1908303@users.noreply.github.com>
2023-01-10 10:48:26 +0100

    Update Finding a Spliced Motif.py

--- Finding a Spliced Motif.py
+++ Finding a Spliced Motif.py
@@ -1,1 +1,11 @@
+from data_reader import fasta_reader
 
+sequence = fasta_reader("rosalind_sseq.txt")
+seq1 = str(sequence[0])
+seq2 = str(sequence[1])
+index = [0]
+for char in seq2:
+    index.append(seq1[sum(index):].find(char))
+    with open("zzzzresult.txt", "a") as done:
+        done.write(str(sum(index)+1))
+        done.write(" ")
